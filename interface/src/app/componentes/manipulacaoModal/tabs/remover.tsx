a2fff2160e08c1dd4d776deb2310bbbb34c8d78c
Eduardo Brito <83357278+EduardoABrito@users.noreply.github.com>
2023-06-11 22:57:41 -0300

    tela de remover

--- interface/src/app/componentes/manipulacaoModal/tabs/remover.tsx
+++ interface/src/app/componentes/manipulacaoModal/tabs/remover.tsx
@@ -1,5 +1,5 @@
 import { Button, Grid, TextField } from "@mui/material";
-import { FaTrash } from 'react-icons/fa'
+import { FaTrashAlt } from 'react-icons/fa'
 export default function RemoverTab(){
     return(
         <Grid container spacing={2}>
@@ -10,7 +10,7 @@ export default function RemoverTab(){
                 <TextField id="id-vertice-origem" label="Aresta" variant="standard" color="info" fullWidth/>
             </Grid>
             <Grid item md={12}>
-                <Button color="error" variant="outlined" fullWidth startIcon={<FaTrash/>}>Remover</Button> 
+                <Button color="error" variant="outlined" fullWidth startIcon={<FaTrashAlt/>}>Remover</Button> 
             </Grid>
         </Grid>
     )
